507120ddbc1374a10dc1a5a0ed9a476426d23f18
levi <levi@levibrooke.com>
2018-01-03 18:10:55 -1000

    Add css to .cell, createGrid function to js

--- js/PixelPainter.js
+++ js/PixelPainter.js
@@ -9,24 +9,27 @@ document.getElementById("pixelPainter").appendChild(createDiv);
 //Create grid
 var pixelPainter = function(width, height) {
 
-
-    
 }
 
-    function createGrid(width, height) {
-        for (let i = 0; i < width; i++) {
-            createDiv.setAttribute("class", "cell");
-           // return "test";
+// Create a grid
 
-            for (let j = 1; j < height; j++) {
-                createDiv.setAttribute("class", "cell");
-                console.log("test");
-            }
-        }
-    }
+function createGrid(row, column) {
     
+  
 
+  
+  for (let i = 0; i < row; i++) {
 
-//Create color grid
-//two buttons
-//
+    let row = document.createElement("div");
+    row.setAttribute("class", "row");
+    row.setAttribute("id", "row" + [i]);
+    document.querySelector(".col").appendChild(row);
+      
+      for (let j = 0; j < column; j++) {
+
+        let cell = document.createElement("div");
+        cell.setAttribute("class", "cell");
+        document.querySelector("#row" + [i]).appendChild(cell);
+      }
+    }
+}
