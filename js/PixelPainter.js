011eef2ded7ea589fa40c6d114838922cc4e200e
KrishnaP <kpat132@gmail.com>
2018-01-04 11:33:02 -1000

    this is the old draft

--- js/PixelPainter.js
+++ js/PixelPainter.js
@@ -13,17 +13,17 @@ var pixelPainter = function(width, height) {
     
 }
 
-    function createGrid(width, height) {
-        for (let i = 0; i < width; i++) {
-            createDiv.setAttribute("class", "cell");
-           // return "test";
+function createGrid(width, height) {
+  for (let i = 0; i < width; i++) {
+    createDiv.setAttribute("class", "cell");
+    // return "test";
 
-            for (let j = 1; j < height; j++) {
-                createDiv.setAttribute("class", "cell");
-                console.log("test");
-            }
-        }
+    for (let j = 1; j < height; j++) {
+      createDiv.setAttribute("class", "cell");
+      console.log("test");
     }
+  }
+}
     
 
 
