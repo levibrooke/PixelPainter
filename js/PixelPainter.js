3b2e6bfc77652ae7968cfbef3e601b466b0b3da6
KrishnaP <kpat132@gmail.com>
2018-01-03 16:21:29 -1000

    grid

--- js/PixelPainter.js
+++ js/PixelPainter.js
@@ -1,1 +1,32 @@
-//adfcgvhbnjkm;
+//Structure
+
+
+let createDiv = document.createElement("div");
+createDiv.setAttribute("class", "col");
+
+document.getElementById("pixelPainter").appendChild(createDiv);
+
+//Create grid
+var pixelPainter = function(width, height) {
+
+
+    
+}
+
+    function createGrid(width, height) {
+        for (let i = 0; i < width; i++) {
+            createDiv.setAttribute("class", "cell");
+           // return "test";
+
+            for (let j = 1; j < height; j++) {
+                createDiv.setAttribute("class", "cell");
+                console.log("test");
+            }
+        }
+    }
+    
+
+
+//Create color grid
+//two buttons
+//
