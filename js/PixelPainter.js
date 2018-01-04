49245a4f0c8b01ace4e31cc44cbd172fbcab17c2
levi <levi@levibrooke.com>
2018-01-03 18:54:35 -1000

    Add event listener for color picker

--- js/PixelPainter.js
+++ js/PixelPainter.js
@@ -12,24 +12,47 @@ var pixelPainter = function(width, height) {
 }
 
 // Create a grid
+function createGrid(row, column, parent) {
 
-function createGrid(row, column) {
-    
-  
+  let cellCount = 0;
 
-  
   for (let i = 0; i < row; i++) {
 
     let row = document.createElement("div");
     row.setAttribute("class", "row");
     row.setAttribute("id", "row" + [i]);
-    document.querySelector(".col").appendChild(row);
+    document.querySelector(parent).appendChild(row);
       
       for (let j = 0; j < column; j++) {
 
         let cell = document.createElement("div");
         cell.setAttribute("class", "cell");
+        cellCount++;
+        cell.setAttribute("id", "cell" + cellCount);
         document.querySelector("#row" + [i]).appendChild(cell);
       }
     }
+}
+
+// Create Color Picker Grid
+createGrid(2,2,".col");
+
+// Color Picker 
+
+// Add event listener 
+var colorPickerCells = document.querySelectorAll(".col .cell");
+
+function colorPickerListener(arr) {
+  for (i = 0; i < arr.length; i++) {
+    arr[i].addEventListener("click", thisFunction);
+  }
+}
+colorPickerListener(colorPickerCells);
+
+// for (i = 0; i < colorPickerCells.length; i++) {
+//   colorPickerCells[i].addEventListener("click", thisFunction);
+// }
+
+function thisFunction() {
+  console.log("Successful event listener!")
 }
